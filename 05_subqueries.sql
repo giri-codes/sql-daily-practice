f859b68e56da0cfea74f300a0aa4800ca567234f
Gireesh L B <gireeshlb888@gmail.com>
2026-03-11 07:41:49 +0530

    Add Day 5 subqueries practice

--- 05_subqueries.sql
+++ 05_subqueries.sql
@@ -42,3 +42,53 @@ WHERE customer_id IN (
     FROM orders
 );
     
+    
+-- Practice Examples:
+-- Q1: Orders Greater Than Average Order Amount
+-- (Subquery with AVG)
+SELECT * FROM orders
+WHERE amount > (
+	SELECT AVG(amount)
+    FROM orders
+);
+
+-- Q2: Customer Who Placed the Highest Order
+-- (Subquery with MAX)
+SELECT name
+FROM customers
+WHERE customer_id = (
+	SELECT customer_id
+    FROM orders
+    WHERE amount = (
+		SELECT MAX(amount)
+        FROM orders
+	)
+);
+
+-- Q3: Customers Who Placed Orders
+-- (Subquery with IN)
+SELECT name
+FROM customers
+WHERE customer_id IN (
+	SELECT customer_id
+    FROM orders
+);
+
+-- Q4: Customers Who Never Placed Orders
+-- (Subquery with NOT IN)
+SELECT name
+FROM customers
+WHERE customer_id NOT IN (
+	SELECT customer_id
+    FROM orders
+);
+
+-- Q5: Orders From Customers Living In Bangalore
+-- (Subquery with condition)
+SELECT * 
+FROM orders
+WHERE customer_id IN (
+	SELECT customer_id
+    FROM customers
+    WHERE city = 'Bangalore'
+);
